bc8bdecf58f68dbf2b2552b36f7727d8942d79ac
Denys21ak <80395255+Denys21ak@users.noreply.github.com>
2023-01-07 20:59:33 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:1,
+		autoplay:true,
+		speed:2000,
+		autoplaySpeed:3000,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
+
